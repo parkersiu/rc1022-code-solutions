24c3d12e16ced20bb3b334de243cdc2e6a3b3e81
Parker Siu <parker.siu@gmail.com>
2022-10-18 16:31:30 +0000

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,54 @@
+var orderHistory = [
+  {
+    orderNumber: '11439416898772232',
+    orderDate: 'August 4, 2020',
+    total: '$34.00',
+    shipTo: 'JS Masher',
+    deliveryDate: 'August 8, 2020',
+    item1: 'JavaScript for impatient programmers',
+    item2: '',
+    returnWindow1: 'September 7, 2020',
+    returnWindow2: '',
+    price1: '$31.55',
+    price2: ''
+  },
+  {
+    orderNumber: '11399842681280257',
+    orderDate: 'July 19, 2020',
+    total: '$44.53',
+    shipTo: 'JS Masher',
+    deliveryDate: 'July 20, 2020',
+    item1: 'The Timeless Way of Building',
+    item2: '',
+    returnWindow1: 'August 19, 2020',
+    returnWindow2: '',
+    price1: '$41.33',
+    price2: ''
+  },
+  {
+    orderNumber: '11428755579059409',
+    orderDate: 'July 4, 2020',
+    total: '$17.22',
+    shipTo: 'JS Masher',
+    deliveryDate: 'July 7, 2020',
+    item1: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    item2: '',
+    returnWindow1: 'August 5, 2020',
+    returnWindow2: '',
+    price1: '$15.98',
+    price2: ''
+  },
+  {
+    orderNumber: '11328831772648248',
+    orderDate: 'July 3, 2020',
+    total: '$138.93',
+    shipTo: 'JS Masher',
+    deliveryDate: 'July 5, 2020',
+    item1: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+    item2: 'The Art of Sql',
+    returnWindow1: 'August 4, 2020',
+    returnWindow2: 'August 4, 2020',
+    price1: '$94.95',
+    price2: '$33.99'
+  }
+];
