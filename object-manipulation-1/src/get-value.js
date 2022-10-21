46fcd37f7ee28df92d0508e4fb5f227a77cf5b3f
Parker Siu <parker.siu@gmail.com>
2022-10-21 20:05:37 +0000

    Completed object-manipulation-1 challenge.

---
+++ object-manipulation-1/src/get-value.js
@@ -0,0 +1,9 @@
+/* exported getValue */
+// define a new function: getValue with 2 parameters: object and key
+// use a for in loop to get each key in the object
+// return the object at index key
+function getValue(object, key) {
+  return object[key];
+}
+// define a new function: getValue with 2 parameters: object and key
+// return the value of the key property of the object
