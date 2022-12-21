54f52265a5c994f027658e5bf822cf62ffebcc32
Parker Siu <parker.siu@gmail.com>
2022-12-21 23:06:21 +0000

    Completed api-authorization exercise.

---
+++ api-authorization/server/authorization-middleware.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken'); // eslint-disable-line
+const ClientError = require('./client-error'); // eslint-disable-line
+
+function authorizationMiddleware(req, res, next) {
+  try {
+    const accessToken = req.headers['x-access-token'];
+    if (!accessToken) {
+      throw new ClientError(401, 'authentication required');
+    }
+    const payload = jwt.verify(accessToken, process.env.TOKEN_SECRET);
+    req.user = payload;
+    next();
+  } catch (err) {
+    next(err);
+  }
+}
+
+module.exports = authorizationMiddleware;
