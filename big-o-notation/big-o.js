3162b9dec8d3225abc6c8971a48a0315d71390b8
Parker Siu <parker.siu@gmail.com>
2023-01-10 00:12:48 +0000

    Fixed solution.

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -9,9 +9,9 @@ function uniqueLinear(words) {
     i++                             // 2 * n = O(2n)
   ) {
     const word = words[i];          // 2 * n = O(2n)
-    if (!seen[word]) {              // 1 * n = O(n)
-      seen[word] = true;            // 2 * n = O(2n)
-      unique[unique.length] = word; // 3 * n = O(3n)
+    if (!seen[word]) {              // 2 * n = O(2n)
+      seen[word] = true;            // 1 * n = O(n)
+      unique[unique.length] = word; // 2 * n = O(2n)
     }
   }
   return unique;                    // 1 * 1 = O(1)
@@ -25,19 +25,19 @@ function uniqueQuadratic(words) {
     i++                             // 2 * n = O(2n)
   ) {
     const word = words[i];          // 2 * n = O(2n)
-    let isUnique = true;            // 1 * 1 = O(1)
+    let isUnique = true;            // 1 * n = O(n)
     for (
       let c = 0;                    // 1 * n = O(n)
       c < i;                        // 2 * n * n = O(2n^2)
       c++                           // 2 * n * n = O(2n^2)
     ) {
-      const comparing = words[c];   // 2 * n * n = O(?)
+      const comparing = words[c];   // 2 * n * n = O(2n^2)
       if (comparing === word) {     // 1 * n * n = O(n^2)
-        isUnique = false;           // 1 * 1 = O(1)
+        isUnique = false;           // 1 * n * n = O(n^2)
       }
     }
     if (isUnique) {                 // 1 * n = O(n)
-      unique[unique.length] = word; // 3 * n = O(3n)
+      unique[unique.length] = word; // 2 * n = O(2n)
     }
   }
   return unique;                    // 1 * 1 = O(1)
